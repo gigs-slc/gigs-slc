d168491cbf6f1951391bf6b7e879b89663d0c56e
griffin <griffin@sportscashin.com>
2020-03-25 10:50:41 -0600

    adding a theme

--- index.js
+++ index.js
@@ -1,5 +1,44 @@
-import { AppRegistry } from 'react-native';
+import * as React from 'react';
+import { configureFonts, DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
 import App from './App';
-import { name as appName } from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+const fontConfig = {
+  default: {
+    regular: {
+      fontFamily: 'sans-serif',
+      fontWeight: 'normal',
+    },
+    medium: {
+      fontFamily: 'sans-serif-medium',
+      fontWeight: 'normal',
+    },
+    light: {
+      fontFamily: 'sans-serif-light',
+      fontWeight: 'normal',
+    },
+    thin: {
+      fontFamily: 'sans-serif-thin',
+      fontWeight: 'normal',
+    },
+  },
+};
+const theme = {
+  ...DefaultTheme,
+  roundness: 2,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: '#288AFD',
+    accent: '#191919',
+  },
+  fonts: configureFonts(fontConfig),
+};
+
+export default function Main() {
+  return (
+    <PaperProvider theme={theme}>
+      <App />
+    </PaperProvider>
+  );
+}
+
+AppRegistry.registerComponent('main', () => Main);
