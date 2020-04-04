53c368884532410f96a7edfc25638fbd31e7e059
hraya <hraya@lssu.edu>
2020-04-03 19:27:34 -0500

    fixed typo

--- firebase.js
+++ firebase.js
@@ -6,7 +6,7 @@ const firebaseConfig = {
     apiKey: API_KEY,
     authDomain: AUTH_DOMAIN,
     databaseURL: DATABASE_URL,
-    projectId: gigs-c0643,
+    projectId: PROJECT_ID,
     storageBucket: STORAGE_BUCKET,
     messagingSenderId: MESSAGE_SENDER_ID,
     appId: APP_ID,
